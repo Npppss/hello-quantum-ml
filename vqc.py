6a02a3ae7218e6ff13afb6df829d799819c66c2d
Npppss <novandrae7980@gmail.com>
2026-08-12 11:07:50 +0700

    update done preparing dataset

--- vqc.py
+++ vqc.py
@@ -14,3 +14,19 @@ from sklearn.preprocessing import  MinMaxScaler
 # Persiapkan Dataset
 print("Persiapan dataset klasik...")
 # Membuat dataset dummy: 100 titik data, 2 fitur (X dan Y), 2 kelas (0 dan 1)
+X, y=make_blobs(n_samples=100, centers=2, n_features=2, random_state=42)
+
+# Normalisasi data (penting untuk rotasi sudut kuantum)
+# Kita skala data antara 0 dan 1, atau -pi hingga pi
+scaler = MinMaxScaler(feature_range=(0, np.pi))
+X_scaled = scaler.fit_transform(X)
+
+# Qiskit VQC mengharapkan label dalam format One-Hot Encoding
+# Kelas 0 -> [1, 0] | Kelas 1 -> [0, 1]
+y_onehot = np.zeros((y.size, y.max() + 1))
+y_onehot[np.arange(y.size), y] = 1
+
+# Membagi data menjadi 80% Training dan 20% Testing
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_onehot, test_size=0.2, random_state=42)
+
+# Arsitektur Kuantum (QPU)
